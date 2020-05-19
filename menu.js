1b406d3a657c4a2ab19dbe8b5a3176c6fc13fa14
smallgear <northwindapps@gmail.com>
2020-05-19 20:19:39 +0900

    first

---
+++ menu.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementsByClassName("sidemenu")[0].style.width = "20vw";
+    document.getElementsByClassName("restmenu")[0].style.width = "80vw";
+  }
+  
+  function closeNav() {
+    document.getElementsByClassName("sidemenu")[0].style.width = "0";
+    document.getElementsByClassName("restmenu")[0].style.width = "0";
+  }
